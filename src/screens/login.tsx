11e4a9d192a8137bb7595bd6f8ecad48ba5918d8
BNK <eufrasiofk07.gmail.com>
2025-11-27 19:47:44 -0300

    feat: add area util

--- src/screens/login.tsx
+++ src/screens/login.tsx
@@ -1,9 +1,9 @@
-import { Text, View } from "react-native"
+import { Text, View } from 'react-native';
 
 export function LoginScreen() {
-    return (
-        <View className="flex-1 justify-center items-center bg-red-400">
-            <Text className="text-6xl">Tela de Loguin</Text>
-        </View>
-    )
+  return (
+    <View className="flex-1 items-center justify-center bg-red-400">
+      <Text className="text-6xl">Tela de Loguin</Text>
+    </View>
+  );
 }
