dbab27eb82b03b95854e3a1c043f0b06a9449def
BNK <eufrasiofk07.gmail.com>
2025-11-28 21:05:12 -0300

    feature: add novas caixas de textos e botao

--- src/components/botaoCustomizado/index.tsx
+++ src/components/botaoCustomizado/index.tsx
@@ -1,5 +1,9 @@
-import { Button, ButtonProps } from 'react-native';
+import { Button, ButtonProps, TextInputProps, View} from 'react-native';
 
 export function ButtonCustomizado({...rest}: ButtonProps) {
-  return <Button {...rest} />;
-}
+  return (
+    <View className="rounded-2xl w-full" >
+        <Button {...rest} />
+    </View>
+    );
+  }
