eccbd9897bbe674b15c562b8b7f8167941d60ae1
BNK <eufrasiofk07.gmail.com>
2025-12-01 20:36:32 -0300

    feat: add campo esqueceu senha

--- src/components/botaoCustomizado/index.tsx
+++ src/components/botaoCustomizado/index.tsx
@@ -2,7 +2,7 @@ import { Button, ButtonProps, TextInputProps, View} from 'react-native';
 
 export function ButtonCustomizado({...rest}: ButtonProps) {
   return (
-    <View className="rounded-2xl w-full" >
+    <View className="rounded-3xl w-full" >
         <Button {...rest} />
     </View>
     );
--- src/screens/login.tsx
+++ src/screens/login.tsx
@@ -5,10 +5,15 @@ import { Text, View } from 'react-native';
 export function LoginScreen() {
   return (
     <View className="flex-1 items-center justify-center bg-red-400">
-      <InputCustomizado placeholder={'Loguin'} />
-      <InputCustomizado placeholder="Senha" />
+      <View className="w-full items-center justify-center gap-5 p-5">
+        <Text className="text-3xl">Nosso app</Text>
 
-      <ButtonCustomizado title={"Entrar"} />
+        <InputCustomizado placeholder={'Loguin'} />
+        <InputCustomizado placeholder="Senha" />
+
+        <ButtonCustomizado title={'Entrar'} />
+        <Text className="text-sky-500 ">Esqueceu a senha</Text>
+      </View>
     </View>
   );
 }
