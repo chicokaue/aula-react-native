e603872b6f227022a81ac801e15cfbe81624244b
BNK <eufrasiofk07.gmail.com>
2025-12-02 20:48:52 -0300

    feature: add segurança senha

--- src/components/inputCustomizado/index.tsx
+++ src/components/inputCustomizado/index.tsx
@@ -2,7 +2,7 @@ import { TextInput, TextInputProps, View } from 'react-native';
 
 export function InputCustomizado({ ...rest }: TextInputProps) {
   return (
-  <View className="border-gray-500 border-2 rounded-2xl w-full">
+  <View className="border-s-gray-300 border-2 rounded-2xl w-full bg-white">
       <TextInput {...rest} />
   </View>
   );
--- src/screens/login.tsx
+++ src/screens/login.tsx
@@ -1,18 +1,39 @@
 import { ButtonCustomizado } from '@/components/botaoCustomizado';
 import { InputCustomizado } from '@/components/inputCustomizado';
+import { useState } from 'react';
 import { Text, View } from 'react-native';
 
 export function LoginScreen() {
+  const [login, setLogin] = useState('');
+  const [senha, setSenha] = useState('');
+  console.log('login', login, 'senha', senha);
   return (
-    <View className="flex-1 items-center justify-center bg-red-400">
+    <View className="flex-1 items-center justify-center bg-black">
       <View className="w-full items-center justify-center gap-5 p-5">
-        <Text className="text-3xl">Nosso app</Text>
+        <Text className="text-3xl text-white">Nosso app</Text>
 
-        <InputCustomizado placeholder={'Loguin'} />
-        <InputCustomizado placeholder="Senha" />
+        <InputCustomizado
+          placeholder={'Login'}
+          onChangeText={(text) => {
+            setLogin(text);
+          }}
+        />
 
-        <ButtonCustomizado title={'Entrar'} />
-        <Text className="text-sky-500 ">Esqueceu a senha</Text>
+        <InputCustomizado
+          placeholder="Senha"
+          secureTextEntry={true}
+          onChangeText={(text) => {
+            setSenha(text);
+          }}
+        />
+
+        <ButtonCustomizado
+          title={'Entrar'}
+          onPress={() => {
+            console.log("login", login, "senha", senha);
+          }}
+        />
+        <Text className="text-2xl text-sky-500">Esqueceu a senha</Text>
       </View>
     </View>
   );
