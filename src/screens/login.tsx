a808ec3ec762fc1f8f4a419c2fa7cd31b60c2aff
BNK <eufrasiofk07.gmail.com>
2025-12-03 19:55:25 -0300

    feature: add cadastro

--- src/screens/login.tsx
+++ src/screens/login.tsx
@@ -6,11 +6,14 @@ import { Text, View } from 'react-native';
 export function LoginScreen() {
   const [login, setLogin] = useState('');
   const [senha, setSenha] = useState('');
-  console.log('login', login, 'senha', senha);
+
+  function logar() {
+    console.log('login', login, 'senha', senha);
+  }
   return (
-    <View className="flex-1 items-center justify-center bg-black">
+    <View className="flex-1 items-center justify-center bg-white">
       <View className="w-full items-center justify-center gap-5 p-5">
-        <Text className="text-3xl text-white">Nosso app</Text>
+        <Text className="text-3xl text-black">Nosso app</Text>
 
         <InputCustomizado
           placeholder={'Login'}
@@ -30,10 +33,14 @@ export function LoginScreen() {
         <ButtonCustomizado
           title={'Entrar'}
           onPress={() => {
-            console.log("login", login, "senha", senha);
+            console.log('login', login, 'senha', senha);
           }}
         />
-        <Text className="text-2xl text-sky-500">Esqueceu a senha</Text>
+        <View className="flex-row gap-4">
+          <Text className="2xl text-sky-500">Esqueceu a senha </Text>
+          <Text className="2xl text-black">|</Text>
+          <Text className="2xl text-sky-500">Cadastre-se</Text>
+        </View>
       </View>
     </View>
   );
