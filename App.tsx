9ae36ce18725b1ccfc261c3e5570ff4ad390daf8
BNK <eufrasiofk07.gmail.com>
2025-12-18 20:28:48 -0300

    feat: add navega

--- App.tsx
+++ App.tsx
@@ -15,8 +15,8 @@ export default function App() {
     <>
       <SafeAreaView className="flex-1">
         <NavigationContainer>
-          <Stack.Navigator>
-            <Stack.Screen name="Login" component={LoginScreen} />
+          <Stack.Navigator initialRouteName='login' screenOptions={{headerShown: false}}>
+            <Stack.Screen name="login" component={LoginScreen} />
             <Stack.Screen name="soma" component={Soma} />
             <Stack.Screen name="listas" component={ListaScreen} />
           </Stack.Navigator>
--- src/interface/rootPages.tsx
+++ src/interface/rootPages.tsx
@@ -1,7 +1,7 @@
 import { NativeStackNavigationProp } from "@react-navigation/native-stack";
 
 export type RootStackParamList = {
-  loguin: undefined;
+  login: undefined;
   soma: undefined;
   listas: undefined;
 };
